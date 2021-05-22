551e3ba63e2310a840dd74d5ac91ea9c5c17a371
hongyura <fkdl55@hs.ac.kr>
2021-05-22 22:48:38 +0900

    수소차 개요 페이지, 관련 사이트, static 폴더

--- main_page/urls.py
+++ main_page/urls.py
@@ -6,4 +6,6 @@ app_name = 'main_page'
 urlpatterns = [
     path('', main_page.views.home, name='home'),
     path('inquire/', main_page.views.inquire, name='inquire'),
+    path('H_car_recap/', main_page.views.H_car_recap, name='H_car_recap'),
+    path('Related_Sites/', main_page.views.Related_Sites, name='Related_Sites'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def inquire(request):
     return render(request, "inquire.html")
+
+def H_car_recap(request):
+    return render(request, "H_car_recap.html")
+
+def Related_Sites(request):
+    return render(request, "Related_Sites.html")
