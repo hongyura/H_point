5f38fa425976089606a7c436fd1857a60a734e2f
hongyura <fkdl55@hs.ac.kr>
2021-05-23 02:45:48 +0900

    커밋합니다

--- main_page/views.py
+++ main_page/views.py
@@ -7,14 +7,13 @@ def home(request):
 def inquire(request):
     return render(request, "inquire.html")
 
-<<<<<<< HEAD
 def H_car_recap(request):
     return render(request, "H_car_recap.html")
 
 def Related_Sites(request):
     return render(request, "Related_Sites.html")
-=======
+
 def H_car_charge(request):
     return render(request, "H_car_charge.html")
 
->>>>>>> cdbcfd7e6b324b3bf7929b798ccbbcf81d63e64b
+
